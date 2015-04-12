3bd65507e9fcaf4a0230ef415248e9c3b9a98fcf
Doug Gandle <dgandle@umass.edu>
2015-04-11 23:12:49 -0400

    Updated to latest version of html, css, js-- working sliders

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
 
     var panelOne = $('#panelOne');
-    var panelTwo;
+    var panelTwo = $('#panelTwo');
+    var panelThree = $('#panelThree');
 
-    //var openPanel = panelOne;
-
+    $( ".selector" ).autocomplete({
+    position: { my : "center"}
+    });
+    
+    //LINK HOVERS
     $(document).on("mouseenter", ".open ul li a", function() {
 	$(this).css("background", "#999999");
 	$(this).css("color", "#fff");
@@ -12,31 +16,72 @@ $(document).ready(function() {
     $(document).on("mouseleave", ".open ul li a", function() {
 	$(this).css("background", "#eeeeee");
 	$(this).css("color", "#999");
+    });
+    
+    
+    //CLOSED PANEL HOVERS
+    $(document).on("mouseenter", ".closed", function() {
+	$(this).fadeTo(100, 0.8);
+    });
+    $(document).on("mouseleave", ".closed", function() {
+	$(this).fadeTo(100, 0.5);
     })
     
+    
+    //PANEL OPENINGS
     $(document).on("click", "#panelOne.open ul li a", function() {
-	    panelTwo = panelOne.clone();
-	    panelTwo.attr("id", "panelTwo");
-	    panelOne.after(panelTwo);
+        //PANEL MOVEMENT
+	panelTwo.fadeIn({queue: false, duration: 250});
+	panelTwo.animate({left: "+=200px"}, 250);
+        panelOne.fadeTo(300, 0.5);
+        
+        panelOne.attr("class", "panel closed");
+        panelTwo.attr("class", "panel open");
+	
+	//PANEL DATA POPULATION
+	$.getJSON("catalog.json", function(data) {
 	    
-	    panelTwo.animate({left: "+=100px"}, 250);
-	    panelOne.fadeTo(300, 0.6);
-	    
-	    panelOne.attr("class", "panel closed");
-	    panelTwo.attr("class", "panel open");
+	});
     });
     
     $(document).on("click", "#panelTwo.open ul li a", function() {
-	    var panelThree = panelTwo.clone();
-	    panelThree.attr("id", "panelThree");
-	    panelTwo.after(panelThree);
-	    
-	    panelThree.animate({left: "+=100px"}, 250);
-	    panelTwo.fadeTo(300, 0.6);
-	    panelOne.fadeTo(300, 0.3);
-	    
-	    panelTwo.attr("class", "panel closed");
-	    panelThree.attr("class", "panel open");
+	panelThree.fadeIn({queue: false, duration: 250});
+        panelThree.animate({left: "+=200px"}, 250);
+        panelTwo.fadeTo(300, 0.5);
+        panelOne.fadeTo(300, 0.5);
+        
+        panelTwo.attr("class", "panel closed");
+        panelThree.attr("class", "panel open");
     });
     
+    
+    //PANEL CLOSINGS
+    $(document).on("click", "#panelTwo.closed", function() {
+	panelThree.animate({left: "+=200px", opacity: "0"}, 250)
+	panelThree.attr("class", "panel closed");
+	panelTwo.attr("class", "panel open");
+	panelTwo.fadeTo(250, 1);
+    });
+    
+    $(document).on("click", "#panelOne.closed", function() {
+	if (panelThree.is(".open")) {
+	    panelThree.fadeOut({queue: false, duration: 250});
+	    panelThree.animate({left: "+=200px"}, 250);
+	    panelThree.attr("class", "panel closed");
+	    
+	    panelTwo.fadeOut({queue: false, duration: 250});
+	    panelTwo.animate({left: "+=200px"}, 250);
+	    panelTwo.attr("class", "panel closed");
+	    
+	    panelOne.fadeTo(250, 1);
+	    panelOne.attr("class", "panel open");
+	} else {
+	    panelTwo.fadeOut({queue: false, duration: 250});
+	    panelTwo.animate({left: "+=200px"}, 250);
+	    panelTwo.attr("class", "panel closed");
+	    
+	    panelOne.fadeTo(250, 1);
+	    panelOne.attr("class", "panel open");
+	}
+    });
 });
