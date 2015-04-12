4ada3ac7407befef14900b955169c637eb1878fc
cosileone <cosi.leone@icloud.com>
2015-04-12 12:53:08 -0400

    submit button now goes to /main endpoint

--- application.py
+++ application.py
@@ -3,9 +3,15 @@ from flask import Flask, render_template
 application = app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def login():
 	return render_template("login.html")
 
+
+@app.route("/main", methods=['GET', 'POST'])
+def index():
+	return render_template("main.html")
+		
+		
 if __name__ == "__main__":
 	host = "127.0.0.1"
 	port = 5000
