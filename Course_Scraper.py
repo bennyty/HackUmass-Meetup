4ed51fd756febabec4af4071fa12072dc53ec2fd
Cheyan Setayesh <cheyan.set@gmail.com>
2015-04-12 02:12:07 -0400

    Added comments and removed useless code, added sample tests for json blob

--- Course_Scraper.py
+++ Course_Scraper.py
@@ -1,7 +1,6 @@
 import json
 from lxml import html
 import requests
-
 import re
 from bs4 import BeautifulSoup
 import string
@@ -11,12 +10,13 @@ import xml
 import sys
 import itertools
 
-
+#TODO - fix hardcoded urls
 BASE_URL = "https://cesd3.oit.umass.edu/undergradguide/2014-2015/"
 MAIN_PAGE = "Chapter2298.html"
 ABBR_PAGE = "http://www.umass.edu/ug_programguide/academicinfo/abbreviationsforcourses.html"
 major_map = {}
 abrreviation_map = {}
+#ignores SSL warnings cause they're fucking annoying
 requests.packages.urllib3.disable_warnings()
 
 class Course_Scraper():
@@ -34,12 +34,19 @@ class Course_Scraper():
         major_map.pop('departmental honors',None)
        	major_map.pop('',None)
 
+       	json_data = open("document.json").read()
+       	data = json.loads(json_data)
+       	pprint(data)
+       	list = (data["CMPSCI"].keys())
+       	print type(list[0][1])
        	#populates major_map with hashmap of courses
        	#populates abrreviation_map with hashmap of abbreviations
        	abrreviation_map = self.getAbbreviation()
-       	print abrreviation_map
+       	#print abrreviation_map
        	for major,link in major_map.items():
        		major_map[major] = self.getCourses(major,link)
+       	#self.writeBlob()
+
 
     def getCourses(self,course,link):
     	"""Gets courses assosciated with given major"""
@@ -56,106 +63,89 @@ class Course_Scraper():
 
     	page = requests.get(BASE_URL + link,verify=False)
     	soup = BeautifulSoup(page.content)
-    	#printContent = soup.findAll('div', {"class" : "printcontent"})
     	regex = re.compile(r'<p><strong>(\w+)')
     	uniqueTup = []
     	normalTup = []
     	courseList = []
+
+    	#
     	for link in soup.find_all("p"):
-    		#print str(link) + "\n"
+
+    		#special cases
     		if '<br/><br/>' in str(link):
-    			#print link
+    			#tuple {major:[course_number,name]}
     			tup = self.uniqueCases(link)
     			if tup:
     				uniqueTup = tup
+    		#standard case
     		elif re.match(regex,str(link)):
+    			#tuple {major:[course_number,name]}
     			tup = normalCase = self.normalCase(link)
     			if tup:
     				normalTup = tup
+    		#puts same major tuples under 1 major
     		courseTuple = uniqueTup + normalTup
-    		#if courseTuple
     		courseList.append(courseTuple)
     	return courseList
-    		#return courseTuple
-    			#course = str(link)[11:].strip('</strong>').strip('<strong>').strip('<br/>').strip('cr').strip('</p')
-    			#course = ''.join(xml.etree.ElementTree.fromstring(str(link)).itertext()).encode('utf-8')
-    			#courseNumber = re.search(r'([^\s]+)',course)
-    			#if courseNumber:
-    			#	courseNumber = courseNumber.group(0).strip()
-    			#courseName = re.search(r'^[^\(]+',course.strip(courseNumber))
-    			#if courseName:
-    			#	courseName = courseName.group(0).strip()
-    			#print course
-    			#print courseNumber
-    			#print courseName
 
+    #runs normal case after splitting special case into seperate courses
     def uniqueCases(self,link):
-    	#print str(link) + "\n"
     	for course in link.find_all('br'):
-    		#print course
     		return self.normalCase(course)
 
     def normalCase(self,link):
-    	#regex = re.compile(r'<p><strong>(\w+)')
-    	#if re.match(regex,str(link)):
-    		#course = str(link)[11:].strip('</strong>').strip('<strong>').strip('<br/>').strip('cr').strip('</p')
+    	#removes html tags
     	course = ''.join(xml.etree.ElementTree.fromstring(str(link)).itertext()).encode('utf-8')
     	courseNumber = re.search(r'([^\s]+)',course)
     	if courseNumber:
     		courseNumber = [courseNumber.group(0).strip()]
+    		
     		commaCase = re.search(r'\d*[,]\d*',courseNumber[0])
+    		#if no course number, ignore
     		if not any(char.isdigit() for char in courseNumber):
     			return None
     		elif commaCase:
     			multipleCourseNumList = course.split(',')
+    			#split up list by comma except for last number which is attached to the course name
     			for number in multipleCourseNumList[:-1]:
     				courseNumber.append(number.strip(','))
-    				#print courseNumber
+    			#special parses for last name in list
     			lastNumber = re.search(r'([^\s]+)',multipleCourseNumList[len(multipleCourseNumList)-1])
     			if lastNumber.group(0):
     				courseNumber.append(str((lastNumber).group(0)).strip())
-    				#print courseNumber
-
-    			#courseNumber.append(lastNumber)
-    			#print commaCase.group(1)
     	courseStripped = None
     	courseName = None
-    	#courseNumber = [courseNumber]
-    	#print courseNumber
+    	#if a course number exists strip all the course numbers, could be 1 or many
     	if courseNumber:
-    		#print courseNumber
-    		#print courseNumber
     		for stripVal in courseNumber:
-    			#print stripVal
     			courseStripped = course.strip(stripVal)
-    	#print courseStripped
     	if courseStripped:
     		courseName = re.search(r'^[^\(]+',courseStripped)
+    	#gets course name if exists
     	if courseName:
     		if courseName.group(0):
+    			#fix special cases
     			courseName = courseName.group(0).strip('OIM').strip('- ').strip()
     			courseName = filter(lambda x: x in string.printable, courseName)
-    	#print courseName
-    	#print courseNumber
+    	#returns tuple of courseNumber and courseName
     	if courseName and courseNumber:
     		return zip(courseNumber,[courseName])
-    	#print course
 
     def getAbbreviation(self):
        	page = requests.get(ABBR_PAGE,verify=False)
         soup = BeautifulSoup(page.content)
-
-        #populates major map with all links assosciated with each major
+        #creates dictionary of abbreviation
         abbrev_map = {}
         for link in soup.find_all("p"):
+
     		link = ''.join(xml.etree.ElementTree.fromstring(str(link)).itertext()).encode('utf-8')
     		abbrevPair = link.split()
+        	#puts abbreviations into tuple and returns it
     		abbrev_map = dict(itertools.izip_longest(*[iter(abbrevPair)] * 2, fillvalue=""))
     		return abbrev_map
     
-
-    		
-
+    #def writeBlob(self):
+    #	with open('catalog.json')
 
 if __name__ == '__main__':
     scraper = Course_Scraper()
