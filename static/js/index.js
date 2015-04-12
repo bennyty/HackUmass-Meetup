5d48f22330bb6f956db32bd01c9178cbf0c2d949
Doug Gandle <dgandle@umass.edu>
2015-04-11 21:23:29 -0400

    Updated index.html, index.css, index.js (newest - beige menu, working slider, highlights selections)

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,42 @@
 $(document).ready(function() {
-    var openPanel = $('#one');
-    $(document).on("click", openPanel.attr('.class1'), function() {
-        var newPanel = openPanel.clone();
-	$('div').after(newPanel);
-        if (openPanel.val("id") == "two") {
-            newPanel.attr("id", "three");
-        } else {
-            newPanel.attr("id", "two");
-        }
-	newPanel.animate({left: "+=100px"}, 250);
-        openPanel = newPanel;
+
+    var panelOne = $('#panelOne');
+    var panelTwo;
+
+    //var openPanel = panelOne;
+
+    $(document).on("mouseenter", ".open ul li a", function() {
+	$(this).css("background", "#999999");
+	$(this).css("color", "#fff");
     });
+    $(document).on("mouseleave", ".open ul li a", function() {
+	$(this).css("background", "#eeeeee");
+	$(this).css("color", "#999");
+    })
+    
+    $(document).on("click", "#panelOne.open ul li a", function() {
+	    panelTwo = panelOne.clone();
+	    panelTwo.attr("id", "panelTwo");
+	    panelOne.after(panelTwo);
+	    
+	    panelTwo.animate({left: "+=100px"}, 250);
+	    panelOne.fadeTo(300, 0.6);
+	    
+	    panelOne.attr("class", "panel closed");
+	    panelTwo.attr("class", "panel open");
+    });
+    
+    $(document).on("click", "#panelTwo.open ul li a", function() {
+	    var panelThree = panelTwo.clone();
+	    panelThree.attr("id", "panelThree");
+	    panelTwo.after(panelThree);
+	    
+	    panelThree.animate({left: "+=100px"}, 250);
+	    panelTwo.fadeTo(300, 0.6);
+	    panelOne.fadeTo(300, 0.3);
+	    
+	    panelTwo.attr("class", "panel closed");
+	    panelThree.attr("class", "panel open");
+    });
+    
 });
