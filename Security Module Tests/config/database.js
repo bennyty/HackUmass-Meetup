9b9d0d2bc64722c71eac652eb481290a55541049
Cheyan Setayesh <cheyan.set@gmail.com>
2015-04-26 02:43:54 -0400

    removed extra modules

--- Security Module Tests/config/database.js
+++ Security Module Tests/config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
 
-  // url : mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot --> using modulus
-  // url : mongodb://localhost/db_name --> using local mongodb
-  'url' :
+  // 'url' : 'mongodb://<user>:<pass>@proximus.modulusmongo.net:27017/aheqE3na' --> using modulus
+  // 'url' : 'mongodb://localhost/db_name --> using local mongodb'
+  'url' : ''
 };
